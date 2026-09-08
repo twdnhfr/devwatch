61031ebd25bfb93af4588eadc97d63b61703db16
Tobi <tobias@wdnhfr.de>
2026-09-08 22:15:53 +0200

    Add DevWatch logo and bundled macOS app icon

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -5,6 +5,15 @@ swift build -c release
 binary_dir="$(swift build -c release --show-bin-path)"
 app_dir="$PWD/build/DevWatch.app"
 mkdir -p "$app_dir/Contents/MacOS"
+mkdir -p "$app_dir/Contents/Resources"
+iconset="$PWD/build/DevWatch.iconset"
+mkdir -p "$iconset"
+for size in 16 32 128 256 512; do
+    sips -z "$size" "$size" Support/Brand/devwatch-logo.png --out "$iconset/icon_${size}x${size}.png" >/dev/null
+    retina_size=$((size * 2))
+    sips -z "$retina_size" "$retina_size" Support/Brand/devwatch-logo.png --out "$iconset/icon_${size}x${size}@2x.png" >/dev/null
+done
+iconutil -c icns "$iconset" -o "$app_dir/Contents/Resources/DevWatch.icns"
 cp "$binary_dir/DevWatch" "$app_dir/Contents/MacOS/DevWatch"
 cp Support/Info.plist "$app_dir/Contents/Info.plist"
 codesign --force --sign - "$app_dir"
